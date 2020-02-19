47740e48f4a329ff473d5daf8bd52f5e40708d4a
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-02-18 21:45:49 -0500

    Commenting code for study

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require("express")
-const helmet = require("helmet")
-const logger = require("morgan")
-const server = express()
-const port = process.env.port || 3095;
+const express = require("express") // Express = application framework atop Node
+const helmet = require("helmet") // Adds https headers for security
+const logger = require("morgan") // logger for inspecting calls
+const server = express() // initialize server
+const port = process.env.port || 3095; // set default port if one is not configured on running machine instance
 const projectRouter = require('./data/routes/projectRouter')
+// Pulling in modular router for Project related inputs
 const actionsRouter = require('./data/routes/actionsRouter')
+// Pulling in modular router for Actions related inputs
 
-server.use(
+server.use( // initializing middleware for availability to server
  helmet(),
  logger('dev'),
- express.json()
+ express.json() // allows for two way parsing of JSON objects
 )
 
-server.use('/api/projects', projectRouter)
+server.use('/api/projects', projectRouter) // Allows server access to request handler for endpoint(s)
 server.use('/api/actions', actionsRouter)
 
-server.listen(port, () => {
+server.listen(port, () => { // initializes server to listen for requests
  console.log(`Server is running live on ${port}`)
 })
--- knexfile.js
+++ knexfile.js
@@ -3,16 +3,16 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'sqlite3', // indicates DB type
     connection: {
       filename: './dev.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
-     directory: './data/migrations'
+     directory: './data/migrations' // pulls in migrations
     },
     seeds: {
-     directory: './data/seeds'
+     directory: './data/seeds' // pulls in seed data
     }
   }
 };
